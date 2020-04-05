f5f995fb2f6e73adff79d58101ce2d86a941f99a
Qi Cao <alanqicao@gmail.com>
2020-04-04 18:15:17 -0600

    Add Car

--- client/src/components/cars/AddCar.js
+++ client/src/components/cars/AddCar.js
@@ -18,25 +18,27 @@ const AddCar = (props) =>{
   const vin = useFormInput ("")
   const policy_number = useFormInput ("")
   const insurance_prov_num = useFormInput ("")
+  const insurance_provider = useFormInput("")
 
     const handleSubmit = e => {
       e.preventDefault()
       axios.post(`/api/users/${user.id}/cars`,{make:make.value,model:model.value,year:year.value,color:color.value,
       license_plate:license_plate.value,policy_exp:policy_exp.value,
       roadside_ass,miles:miles.value,vin:vin.value,policy_number:policy_number.value,
-      insurance_prov_num:insurance_prov_num.value,})
+      insurance_prov_num:insurance_prov_num.value,insurance_provider:insurance_provider.value})
       .then
         (res => props.history.goBack())
       .catch( (err) => {
         console.log(err)
       })
     }
-    
+
     return(
       <Form onSubmit={handleSubmit}>
             <Form.Input
               label="Year"
               autoFocus
+              required
               name='year'
               placeholder='Year'
               {...year}
@@ -44,37 +46,41 @@ const AddCar = (props) =>{
             <Form.Input
               label="Make"       
               name='make'
+              required
               placeholder='Make'
               {...make}
             />
             <Form.Input
               label="Model"
               name='model'
+              required
               placeholder='Model'
               {...model}
             />
             <Form.Input
               label="Color"
               name='color'
+              required
               placeholder='Color'
               {...color}
             />
             <Form.Input
               label="License Plate"
+              required
               name='license_plate'
               placeholder='License Plate'
               {...license_plate}
             />
             <Form.Input
               label="Vin Number"
-              
+              required
               name='vin'
               placeholder='Vin Number'
               {...vin}
             />
             <Form.Input
               label="Miles"
-              
+              required
               name='miles'
               placeholder='Total Miles'
               {...miles}
@@ -95,11 +101,18 @@ const AddCar = (props) =>{
               {...policy_exp}
             />
             <Form.Input
-              label="Insurance Provider"
+              label="insurance Provider Number"
               optional = "true"
               name='insurance_prov_num'
-              placeholder='Insurance Provider'
+              placeholder='Insurance Provider Number'
               {...insurance_prov_num}
+            />
+            <Form.Input
+              label="Insurance Provider"
+              required
+              name='insurance_provider'
+              placeholder='Insurance Provider'
+              {...insurance_provider}
             />
           <Form.Checkbox
             label="Roadside Assistance? Check for yes."
@@ -113,3 +126,5 @@ const AddCar = (props) =>{
 }
 
 export default AddCar
+
+

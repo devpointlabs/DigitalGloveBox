8f56f2d547c3372b5b3980f02656e4a3cfb15bc9
Emily Pena <emilyapena@gmail.com>
2020-04-21 15:36:22 -0600

    Updated car card button, now working and deleted image link

--- client/src/components/cars/Car.js
+++ client/src/components/cars/Car.js
@@ -1,44 +1,37 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import {
-  LeftHalf,
-  Title,
-  Paragraph,
-  CarCard,
-  RightHalf, 
-  ImgCar,
-} from "../styles/DashBoard";
+import { LeftHalf, Title, Paragraph, CarCard, RightHalf, ImgCar, } from "../styles/DashBoard";
 import { Button } from '../../styled_component/styledComponents';
 
-
 const Car = (props) => {
+
   return (
     <>
-        <CarCard key={props.car.id}>
-          <LeftHalf>
-      <Link to={{ pathname: `/car_profile/${props.car.id}` }}>
+      <CarCard key={props.car.id}>
+        <LeftHalf>
           <ImgCar src={`${props.car.file}`} />
-      </Link>
-          </LeftHalf>
-          <RightHalf>
-      <Link to={{ pathname: `/car_profile/${props.car.id}` }}>
+        </LeftHalf>
+        <RightHalf>
+        <Link to={{ pathname: `/car_profile/${props.car.id}` }}>
           <Title style={{display: 'flex', justifyContent: 'center'}}>
             {props.car.year} {props.car.make} {props.car.model}
-            </Title>
-      </Link>
-            <div style={{display: 'flex', flexFlow: 'row wrap', justifyContent: 'center', textAlign: 'center', paddingLeft: '25%',paddingRight: '25%'}}>
-            <Paragraph >{paragraph}</Paragraph>
-            </div>
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-            <Button>Edit Car</Button>
-            </div>
-          </RightHalf>
-        </CarCard>
+          </Title>
+        </Link>
+        <div style={{display: 'flex', flexFlow: 'row wrap', justifyContent: 'center', textAlign: 'center', paddingLeft: '25%',paddingRight: '25%'}}>
+          <Paragraph >{paragraph}</Paragraph>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Link to={{ pathname: `/car_profile/${props.car.id}` }}>
+            <Button>Car Profile</Button>
+          </Link>
+        </div>
+        </RightHalf>
+      </CarCard>
     </>
   );
 };
 
 const paragraph =
-  "All of your cars documents, information, and recent services in one convenient place!";
+  "All of your cars documents, information, and recent services in one convenient place";
 
 export default Car;
